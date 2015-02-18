a18b56416190cf336a30d8125d672c30341a11e3
mhjelmsmark <morten.hjelmsmark@atkinsglobal.com>
2015-02-18 17:17:09 +0100

    esrileafmap version 1.0

--- src/main/webapp/components/esrileafmap/esrileafmap.js
+++ src/main/webapp/components/esrileafmap/esrileafmap.js
@@ -1,29 +1,28 @@
 'use strict';
 
 angular.module('myApp.esrileafmap', ['ngRoute','leaflet-directive'])
+/*
+ * Find better solution for these constants?
+ * */
 .constant('elmconf',{
-<<<<<<< Updated upstream
 	'eprtrLayerUrl':'http://sdkcga6350:6080/arcgis/rest/services/EprtrFacilities/FeatureServer/0',
-=======
-	'eprtrLayerUrl1':'http://sdkcga6350:6080/arcgis/rest/services/EprtrFacilities/FeatureServer/0',
-	'eprtrLayerUrl2':'http://sdkcga6350:6080/arcgis/rest/services/EprtrFacilities/FeatureServer/1',
->>>>>>> Stashed changes
-	'europebounds': [53.526, 10.667]}
-	)
-/*.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/esrileafmap', {
-    templateUrl: 'components/esrileafmap/esrileafmap.html',
-    controller: 'esriLeafMapController'
-  });
-}])*/
+	'europebounds': [53.526, 10.667],
+	'passInFilter': ['offset','limit','order','desc'],
+	'sectors':['0','1','2','3','4','5','6','7','8','9']
+	})
 
-
-.controller('esriLeafMapController', ['$scope', 'leafletData','elmconf', function($scope, leafletData, elmconf) {
+.controller('esriLeafMapController', ['$scope',  'leafletData','elmconf', function($scope,  leafletData, elmconf) {
 	var elm_ctrl = this;
 	var points = [];
+	var where = '';
 	
+	/*
+	 * We extend scope with a collection of icons and two functions
+	 * Icons uses the Leaflet.awsome-markers plugin
+	 * SetWhere function is called when the query parameters are changed after load
+	 * buildWhere function is called when we want to parse the query parameters into a where string
+	 * */
 	angular.extend($scope, {
-		
 		//icon: L.AwesomeMarkers.icon({icon: 'spinner', prefix: 'fa', markerColor: 'red', spin:true}) }).addTo(map);
 		eprtricons: {
 				/*IASectorCode*/
@@ -68,69 +67,92 @@ angular.module('myApp.esrileafmap', ['ngRoute','leaflet-directive'])
 					prefix: 'fa', 
 					markerColor: 'red'})
    	    },
-		where: '',
-		setWhere: function(where_str){
-			if (elm_ctrl.elm_map){
-				var fdlay = elm_ctrl.elm_map.getLayer(1); 
-				fdlay.setWhere(where_str);
-				//$scope.fitToBounds(fdlay.getLayers())
-			}
-			else {
+		setwhere: function(where_str){
+			//console.log('setWhere: ' + where_str);
+			if ($scope.where != where_str){
 				$scope.where = where_str;
-			}
+				if(elm_ctrl.fdlay){
+					elm_ctrl.fdlay.setWhere(where_str);
+					//console.log('elm_ctrl.fdlay: True');
+				}
+				else if(elm_ctrl.elm_map){
+					var fdlay = elm_ctrl.elm_map.getLayer(1); 
+					fdlay.setWhere(where_str);
+				}
+				else {
+					$scope.where = where_str;
+				}			
+			};
 		},
+	    buildWhere: function(queryparams) {
+	    	var arrQue = []
+	    	for (var key in queryparams){
+	    		if (elmconf.passInFilter.indexOf(key)<0){
+	        		var qstr = ""
+	        		if (queryparams[key] && queryparams[key] != ''){
+		        		if (typeof queryparams[key] === 'number'){
+		        			qstr = 	key + "=" + queryparams[key];
+		        		}
+		        		else {        			
+		        			qstr = 	key + "='" + queryparams[key] + "'";
+		        		}	
+		        		arrQue.push(qstr);
+	        		}
+	    		}
+	    	}
+	    	return (arrQue.length > 0)?arrQue.join(" AND "):" ";
+	    },
 	});
-    
-//	$scope.where = $scope.where ? $scope.where : "FacilityReportID=" + $scope.frid; 
-<<<<<<< Updated upstream
-	$scope.where = "CountryCode='NL' AND ReportingYear=2007" //+ $scope.frid; 
-=======
-	$scope.where = "CountryCode='DE' AND ReportingYear=2007" //+ $scope.frid; 
->>>>>>> Stashed changes
 	
+	/*
+	 * if the where string is empty or undefined then we build it 
+	 */
+    if (!$scope.where || $scope.where === '' ){
+        if ($scope.queryparams){
+        	$scope.where = $scope.buildWhere($scope.queryparams);
+        }
+        else if ($scope.wherestr){
+        	$scope.where = $scope.wherestr
+        }
+        else if ($scope.frid){
+        	$scope.where = "FacilityReportID=" + $scope.frid;
+        }
+        else {
+        	$scope.where = " ";
+        }
+    };
+
+    //Internal function to filter min and max coordinates
 	$scope.getMinOrMax = function (feature, minOrMax, latOrLng) {
 		  return _[minOrMax](feature, function (value) {
 		      return value[latOrLng] 
 		  })[latOrLng];
 		};
 	
+	//Internal function for updating the map extent - still not satisfied 
 	$scope.updateExtent = function(features) {
 		  var maxLat = $scope.getMinOrMax(features, "max", "lat");
 		  var minLat = $scope.getMinOrMax(features, "min", "lat");
 		  var maxLng = $scope.getMinOrMax(features, "max", "lng");
 		  var minLng = $scope.getMinOrMax(features, "min", "lng");
-		  console.log('maxLat: ' + maxLat + ', minLat: ' + minLat + ', maxLng: ' + maxLng + ', minLng: ' + minLng);
+		  //console.log('maxLat: ' + maxLat + ', minLat: ' + minLat + ', maxLng: ' + maxLng + ', minLng: ' + minLng);
 		  var southWest = new L.LatLng(minLat, minLng);
 		  var northEast = new L.LatLng(maxLat, maxLng);
 		  return new L.LatLngBounds(southWest, northEast);	
 	};
 
-/*	$scope.fitToBounds = function (features) {
-		  var bounds = $scope.updateExtent(features);
-		  elm_map.fitBounds(bounds,{maxZoom:8});
-  	};*/
-	
+	//Here we initialize the map
 	leafletData.getMap().then(function(map) {
+		//Initial extent
 		map.setView(elmconf.europebounds, 4);
 		elm_ctrl.elm_map = map
+		//We set the baselayer - in version 2 we can add more baselayers and a selector
 		L.esri.basemapLayer("Streets").addTo(map);
 
-		console.log('Que: ' + $scope.where);
-/*		var que = L.esri.Tasks.query({
-			url: elmconf.eprtrLayerUrl2,
-			where: $scope.where
-		});
-		que.count(function(error, count, response){
-		    console.log('Found ' + count + ' features');
-		});
-		que.bounds(function(error, latLngBounds, response){
-		    map.fitBounds(latLngBounds);
-		});*/
-		
-<<<<<<< Updated upstream
+		//Here we define the Clustered Feature Service layer
 		elm_ctrl.fdlay = L.esri.clusteredFeatureLayer(elmconf.eprtrLayerUrl, {
 			spiderfyOnMaxZoom:false,
-		    disableClusteringAtZoom: 12,
+		    disableClusteringAtZoom: 9,
 		    polygonOptions: {
 		      color: '#2d84c8',
 		      weight: 4,
@@ -159,90 +181,61 @@ angular.module('myApp.esrileafmap', ['ngRoute','leaflet-directive'])
    		   	name: "E-PRTR Facilities",
 		    pointToLayer: function (geojson, latlng) {
 			   points.push(latlng)
+			   var sec = geojson.properties.IASectorCode.toLowerCase();
 			   return L.marker(latlng, {
-			        icon: $scope.eprtricons[geojson.properties.IASectorCode.toLowerCase()]
+			        icon: $scope.eprtricons[(elmconf.sectors.indexOf(sec)>0)?sec:'others']
 			      });
 		    },
 		});
-=======
-		elm_ctrl.fdlay = L.esri.featureLayer(elmconf.eprtrLayerUrl1, {
-			where: $scope.where,
-  		   name : "E-PRTR Facilities",
-		   pointToLayer: function (geojson, latlng) {
-			   return L.marker(latlng, {
-			        icon: $scope.eprtricons[geojson.properties.IASectorCode.toLowerCase()]
-			      });
-			   /*return  L.circleMarker(latlng, {
-		          color: '#5B7CBA',
-		          weight: 2,
-		          opacity: 0.85,
-		          fillOpacity: 0.5
-		        }); */
-		    },
-		  });
 
-		elm_ctrl.fdlay.once('load', function(e){
-//			
-			console.log('Once: '+ e.bounds.getCenter());
-			map.panTo(e.bounds.getCenter());
+		//Here we try to navigate to the extent of the selected features - still not satisfied
+		elm_ctrl.fdlay.on('load', function(e){
+//			console.log('pnts: '+ points.length);
+			if (points.length > 0){
+				var bounds = $scope.updateExtent(points);
+	//			console.log('Once: '+ bounds.toBBoxString());
+	  		    map.fitBounds(bounds,{maxZoom:3});
+	  		    points = [];
+			};
+//  		    map.fitBounds(bounds,{maxZoom:2});
 			  // do something on load
 		});
-/*		elm_ctrl.fdlay.query().bounds(function(error, latLngBounds, response){
-			console.log('que: '+ latLngBounds.getCenter());
-		});*/
->>>>>>> Stashed changes
-
+		//Now we add the clusterlayer to the map
 		elm_ctrl.fdlay.addTo(map);
 
-		elm_ctrl.fdlay.once('load', function(e){
-			console.log('pnts: '+ points.length);
-			if (points.length > 0){
-				var bounds = $scope.updateExtent(points);
-				console.log('Once: '+ bounds.toString());
-	  		    map.panTo(bounds);
-			};
-//  		    map.fitBounds(bounds,{maxZoom:2});
-			  // do something on load
-		});
-		
-		elm_ctrl.fdlay.on('load', function(e){
-			console.log('pnts: '+ points.length);
-			if (points.length > 0){
-				var bounds = $scope.updateExtent(points);
-				console.log('Once: '+ bounds.toString());
-//	  		    map.panTo(bounds);
-			};
-//  		    map.fitBounds(bounds,{maxZoom:2});
-			  // do something on load
-		});
-
-/*		elm_ctrl.fdlay.query().bounds(function(error, latLngBounds, response){
-			console.log('que: '+ latLngBounds.getCenter());
-		});*/
-
-
+		//Here we bind a HTML template to the the popup - TODO 
 		elm_ctrl.fdlay.bindPopup(function (feature) {
-<<<<<<< Updated upstream
 				//console.log('pop: '+ feature.properties.X + ' ' + feature.properties.Y);
-=======
-				console.log('pop: '+ feature.properties.X + ' ' + feature.properties.Y);
->>>>>>> Stashed changes
         	    return L.Util.template('<p>{FacilityName }<br>{ReportingYear }<br>{CountryCode }</p>', feature.properties);
         	  });
-
-		//$scope.fitToBounds(elm_ctrl.fdlay.getLayers())
 	});
+	
 	
 }])
 
+/*
+ * This directive enables us to define this module as a custom HTML element
+ * <esri-leaf-map queryparams="" /> 
+ * You can use only one of these parameters: 
+ *    facility-report-id: Interesting in the facilitydetails view where we only focus on one facility
+ *    wherestr: This has given me some problems because Angular wants to pass the string as an expression
+ *    queryparams: a JSON oject with key, value pairs eg. {"ReportingYear":2008, "CountryCode":"NL"}    
+ * */
 .directive('esriLeafMap', function() {
 	return {
-		restrict: 'AE',
+		restrict: 'E',
 		controller: 'esriLeafMapController',
         transclude: true,
-		scope: {frid: '@facilityReportId'},
+		scope: {
+			frid: '@facilityReportId',
+			wherestr: '=',
+			queryparams: '='//,
+		},
 		templateUrl: 'components/esrileafmap/esrileafmap.html',
 		link: function(scope, element, attrs){
+			scope.$watch('queryparams', function() {
+	        	scope.setwhere(scope.buildWhere(scope.queryparams));
+		    },true);
 		}
 	};
 });
--- src/main/webapp/views/facilitydetails/facilitydetails.js
+++ src/main/webapp/views/facilitydetails/facilitydetails.js
@@ -12,7 +12,34 @@ angular.module('myApp.facilitydetails', ['ngRoute','myApp.esrileafmap'])
 .controller('FacilityDetailsController', ['$scope',  function($scope) {
 	$scope.fdtitle = 'Hej Mor'
     $scope.frID = 89352;
+	$scope.objwhere = {
+			cc: '',
+			ye: 2007,
+			wh: ''
+	};
+    $scope.setWhereStr = function(){
+    	$scope.objwhere.wh = 
+    	   {"CountryCode": $scope.objwhere.cc,
+    	   "ReportingYear":$scope.objwhere.ye};
+    }
 
+    if (!$scope.objwhere.wh){
+        $scope.setWhereStr();
+    };
+    
+    $scope.$watch('objwhere.cc', function(value) {
+        console.log('objwhere1: '+ value);
+        $scope.setWhereStr();
+        console.log('objwhere2: '+ JSON.stringify($scope.objwhere.wh));
+    });
+
+    $scope.$watch('objwhere.ye', function(value) {
+        console.log('objwhere3: '+ value);
+        $scope.setWhereStr();
+        console.log('objwhere4: '+ JSON.stringify($scope.objwhere.wh));
+    });
+
+    //$scope.setWhereStr = '';
 	/*esriTestMap.mapelem.addWhereStr('5');
 	esriTestMap.mapelem.addTextStr("Skudder muddr");
 */
