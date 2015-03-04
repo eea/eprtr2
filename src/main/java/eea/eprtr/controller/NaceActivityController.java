4059a6d44660e3410b1b20d6ec4131f2458a09a1
Rolf Thunbo <rthunbo@gmail.com>
2015-03-04 07:59:54 +0100

    Revert "Merge things"
    This reverts commit abdc860a5b442f44eca7f1ff5a577c506db0fa7f.

---
+++ src/main/java/eea/eprtr/controller/NaceActivityController.java
@@ -0,0 +1,41 @@
+package eea.eprtr.controller;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import eea.eprtr.dao.NaceActivityRepository;
+import eea.eprtr.model.NaceActivity;
+
+@RestController
+public class NaceActivityController {
+
+	private NaceActivityRepository repository;
+	
+	@PersistenceContext
+    private EntityManager em;
+
+	@Autowired
+	public NaceActivityController(NaceActivityRepository repository) {
+		this.repository = repository;
+	}
+
+	@RequestMapping("/naceActivity")
+	public List<NaceActivity> list(
+			@RequestParam(value = "ParentID", required = false) Integer parentID) {
+			return repository.list(parentID);
+		}
+
+	@RequestMapping("/naceActivity/{naceCode:.+}")
+	public NaceActivity get(
+		@PathVariable(value = "naceCode") String naceCode) {
+			return repository.get(naceCode);
+	}
+}
---
+++ src/main/java/eea/eprtr/dao/NaceActivityRepository.java
@@ -0,0 +1,44 @@
+package eea.eprtr.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import eea.eprtr.model.NaceActivity;
+
+@Repository
+public class NaceActivityRepository {
+
+	@PersistenceContext
+    private EntityManager em;
+
+	public List<NaceActivity> list(Integer parentID) {
+		Integer startYearEPRTR = new Integer(2007);
+		
+		TypedQuery<NaceActivity> query = null;
+		
+		if (parentID == null) {
+			query = em.createNamedQuery("NaceActivity.findRootActivities", NaceActivity.class);
+		} else {
+			query = em.createNamedQuery("NaceActivity.findActivities", NaceActivity.class);
+			query.setParameter("parentID", parentID);
+		}
+		query.setParameter("startYearEPRTR", startYearEPRTR);
+		List<NaceActivity> results = query.getResultList();
+		return results;
+	}
+
+	public NaceActivity get(String naceCode) {
+		
+		TypedQuery<NaceActivity> query = null;
+		query = em.createNamedQuery("NaceActivity.findByCode", NaceActivity.class);
+		query.setParameter("NaceActivityCode", naceCode);
+		NaceActivity results = query.getSingleResult();
+		return results;
+	}
+
+}
