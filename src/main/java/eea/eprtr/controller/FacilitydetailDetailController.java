22435242479468f16de2e14081bbee2381cbb608
Rolf Thunbo <rthunbo@gmail.com>
2015-03-04 08:08:56 +0100

    import fix

--- src/main/java/eea/eprtr/controller/FacilitydetailDetailController.java
+++ src/main/java/eea/eprtr/controller/FacilitydetailDetailController.java
@@ -3,20 +3,13 @@ package eea.eprtr.controller;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import eea.eprtr.dao.FacilityDetailsDetailsRepository;
-import eea.eprtr.model.FacilitydetailAuthority;
 import eea.eprtr.model.FacilitydetailDetail;
-import eea.eprtr.model.RiverBasinDistrict;
 
 @RestController
 public class FacilitydetailDetailController {
