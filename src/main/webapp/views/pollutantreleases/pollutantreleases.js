a3c7c1dc7e553741caf1925d3505ad8ed85f101c
Rolf Thunbo <rthunbo@gmail.com>
2015-03-04 14:06:16 +0100

    added dummy pie chart

--- src/main/webapp/views/pollutantreleases/pollutantreleases.js
+++ src/main/webapp/views/pollutantreleases/pollutantreleases.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.pollutantreleases', ['ngRoute'])
+angular.module('myApp.pollutantreleases', ['ngRoute', 'googlechart'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/pollutantreleases', {
@@ -13,5 +13,24 @@ angular.module('myApp.pollutantreleases', ['ngRoute'])
         $scope.pollutantPanel = true;
         $scope.showReleasesToInputField = true;
         $scope.pollutantPanelTitle = 'Pollutant releases';
+
+        $scope.search = function() {
+            $scope.searchResults = true;
+            $scope.chartObject = {};
+            $scope.chartObject.data = {"cols": [
+                {id: "t", label: "Name", type: "string"},
+                {id: "s", label: "Total", type: "number"}
+            ], "rows": [
+                {c: [
+                    {v: "5.(f)"},
+                    {v: 107.47}
+                ]},
+                {c: [
+                    {v: "9.(a)"},
+                    {v: 3.59}
+                ]}
+            ]};
+            $scope.chartObject.type = 'PieChart';
+        };
     }])
 ;
